99c50810acfb8f1bd478be1b76893373cdd800d1
mpabarca <mpabarca@uc.cl>
2019-04-09 15:25:08 -0400

    se agrega transicion entre secciones

---
+++ main.js
@@ -0,0 +1,12 @@
+const navbarToggler=document.querySelector(".navbar-toggler");
+const navbarMenu=document.querySelector(".navbar ul");
+const navbarLinks=document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick(){
+    navbarToggler.classList.toggle("open-navbar-toggler");
+    navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
