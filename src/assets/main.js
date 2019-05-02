91c7ff10129300ad8c0da15035d9abff8d85aeed
mpabarca <mpabarca@uc.cl>
2019-05-02 17:02:58 -0400

    pagepiling en css y js funcionando pero faltan ajustes

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,3 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     window.M.Carousel.init(elems, {});
   });
+
+  $(document).ready(function() {
+    $('#pagepiling').pagepiling();
+  });
