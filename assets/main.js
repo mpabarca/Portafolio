767cea029cf1f4f715a561e0400075633d3639d1
mpabarca <mpabarca@uc.cl>
2019-05-04 15:18:05 -0400

    se arregla scroll change menu y se agrega para subir a portafolio

--- assets/main.js
+++ assets/main.js
@@ -9,20 +9,46 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#pagepiling').pagepiling({
       anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
       menu: '#myMenu',
-      navigation:true
-  });
-});
-
-$(function() {
-  var header = $(".navbar");
-
-  $(window).scroll(function() {    
-      var scroll = $(window).scrollTop();
-      if (scroll >= 50) {
-          header.addClass("scrolled");
-      } else {
-          header.removeClass("scrolled");
+      onLeave: function(index, nextIndex, direction){
+        //after leaving section 2
+        
+        if(index == 1 && direction =='down'){
+          document.getElementById("nav-home").style["color"]="#7a2828";
+          document.getElementById("nav-about").style["color"]="#7a2828";
+          document.getElementById("nav-skills").style["color"]="#7a2828";
+          document.getElementById("nav-projects").style["color"]="#7a2828";
+          document.getElementById("nav-contact").style["color"]="#7a2828";
+        }
+        else if(index == 2 && nextIndex== 1 && direction == 'up'){
+          document.getElementById("nav-home").style["color"]="#fff";
+          document.getElementById("nav-about").style["color"]="#fff";
+          document.getElementById("nav-skills").style["color"]="#fff";
+          document.getElementById("nav-projects").style["color"]="#fff";
+          document.getElementById("nav-contact").style["color"]="#fff";
+        }
+        else if(index == 3 && nextIndex== 1 && direction == 'up'){
+          document.getElementById("nav-home").style["color"]="#fff";
+          document.getElementById("nav-about").style["color"]="#fff";
+          document.getElementById("nav-skills").style["color"]="#fff";
+          document.getElementById("nav-projects").style["color"]="#fff";
+          document.getElementById("nav-contact").style["color"]="#fff";
+        }
+        else if(index == 4 && nextIndex== 1 && direction == 'up'){
+          document.getElementById("nav-home").style["color"]="#fff";
+          document.getElementById("nav-about").style["color"]="#fff";
+          document.getElementById("nav-skills").style["color"]="#fff";
+          document.getElementById("nav-projects").style["color"]="#fff";
+          document.getElementById("nav-contact").style["color"]="#fff";
+        }
+        else if(index == 5 && nextIndex== 1 && direction == 'up'){
+          document.getElementById("nav-home").style["color"]="#fff";
+          document.getElementById("nav-about").style["color"]="#fff";
+          document.getElementById("nav-skills").style["color"]="#fff";
+          document.getElementById("nav-projects").style["color"]="#fff";
+          document.getElementById("nav-contact").style["color"]="#fff";
+        }
       }
-  });
 
+  });
 });
+
