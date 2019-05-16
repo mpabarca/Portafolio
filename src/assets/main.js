daa122acd37fd66837e38a0d1e036af0045d8d98
mpabarca <mpabarca@uc.cl>
2019-05-16 14:49:34 -0400

    se modifica main quitando carousel materialize + pagepiling agregando paginas para scroll

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,12 +1,4 @@
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    window.M.Carousel.init(elems, {});
-  });
-
-
-
   $(function() 
   {
       $("#nav-linkedin").mouseenter(function(event) {
@@ -62,14 +54,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     $(document).ready(function() {
       $('#pagepiling').pagepiling({
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage'],
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixPage','sevenPage'],
         menu: '#myMenu',
         navigation: true,
         onLeave: function(index, nextIndex, direction){
           //after leaving section 2
           var widthWindow= window.innerWidth;
-          if((index == 1 && direction =='down'&& nextIndex!=5)
-              ||(index == 5 && direction =='up'&& nextIndex!=1))
+          if((index == 1 && direction =='down'&& nextIndex!=7)
+              ||(index == 7 && direction =='up'&& nextIndex!=1))
               {
             document.getElementById("nav-home").style["color"]="#7a2828";
             document.getElementById("nav-about").style["color"]="#7a2828";
@@ -87,63 +79,21 @@ document.addEventListener('DOMContentLoaded', function() {
               document.getElementById("three-button").style["background"]="#7a2828";
             }
           }
-          else if((index == 2 && nextIndex== 1 && direction == 'up')
-              ||(index == 2 && nextIndex== 5 && direction == 'down')){
-            document.getElementById("nav-home").style["color"]="#fff";
-            document.getElementById("nav-about").style["color"]="#fff";
-            document.getElementById("nav-skills").style["color"]="#fff";
-            document.getElementById("nav-projects").style["color"]="#fff";
-            document.getElementById("nav-contact").style["color"]="#fff";
-            document.getElementById("line-hor").style["border-top"]="2px solid #fff";
-            document.getElementById("line-ver").style["border-right"]="2px solid #fff";
-            document.getElementById("logo").style["background-image"]='url("assets/img/logo.png")';
-            document.getElementById("nav-linkedin").style["color"]="#fff";
-            document.getElementById("nav-github").style["color"]="#fff";
-            if (widthWindow < 1199){
-              document.getElementById("one-button").style["background"]="#fff";
-              document.getElementById("two-button").style["background"]="#fff";
-              document.getElementById("three-button").style["background"]="#fff";
-            }
-            
-          }
-          else if((index == 3 && nextIndex== 1 && direction == 'up')
-              ||(index == 3 && nextIndex== 5 && direction == 'down')){
-            document.getElementById("nav-home").style["color"]="#fff";
-            document.getElementById("nav-about").style["color"]="#fff";
-            document.getElementById("nav-skills").style["color"]="#fff";
-            document.getElementById("nav-projects").style["color"]="#fff";
-            document.getElementById("nav-contact").style["color"]="#fff";
-            document.getElementById("line-hor").style["border-top"]="2px solid #fff";
-            document.getElementById("line-ver").style["border-right"]="2px solid #fff";
-            document.getElementById("logo").style["background-image"]='url("assets/img/logo.png")';
-            document.getElementById("nav-linkedin").style["color"]="#fff";
-            document.getElementById("nav-github").style["color"]="#fff";
-            if (widthWindow < 1199){
-              document.getElementById("one-button").style["background"]="#fff";
-              document.getElementById("two-button").style["background"]="#fff";
-              document.getElementById("three-button").style["background"]="#fff";
-            }
-          }
-          else if((index == 4 && nextIndex== 1 && direction == 'up')
-              ||(index == 4 && nextIndex== 5 && direction == 'down')){
-            document.getElementById("nav-home").style["color"]="#fff";
-            document.getElementById("nav-about").style["color"]="#fff";
-            document.getElementById("nav-skills").style["color"]="#fff";
-            document.getElementById("nav-projects").style["color"]="#fff";
-            document.getElementById("nav-contact").style["color"]="#fff";
-            document.getElementById("line-hor").style["border-top"]="2px solid #fff";
-            document.getElementById("line-ver").style["border-right"]="2px solid #fff";
-            document.getElementById("logo").style["background-image"]='url("assets/img/logo.png")';
-            document.getElementById("nav-linkedin").style["color"]="#fff";
-            document.getElementById("nav-github").style["color"]="#fff";
-            if (widthWindow < 1199){
-              document.getElementById("one-button").style["background"]="#fff";
-              document.getElementById("two-button").style["background"]="#fff";
-              document.getElementById("three-button").style["background"]="#fff";
-            }
-          }
-          else if((index == 5 && nextIndex== 1 && direction == 'up')
-              ||(index == 1 && nextIndex== 5 && direction == 'down')){
+          else if(
+                (index == 2 && nextIndex== 1 && direction == 'up')
+              ||(index == 2 && nextIndex== 7 && direction == 'down')
+              ||(index == 3 && nextIndex== 1 && direction == 'up')
+              ||(index == 3 && nextIndex== 7 && direction == 'down')
+              ||(index == 4 && nextIndex== 1 && direction == 'up')
+              ||(index == 4 && nextIndex== 7 && direction == 'down')
+              ||(index == 5 && nextIndex== 1 && direction == 'up')
+              ||(index == 5 && nextIndex== 7 && direction == 'down')
+              ||(index == 6 && nextIndex== 1 && direction == 'up')
+              ||(index == 6 && nextIndex== 7 && direction == 'down')
+              ||(index == 5 && nextIndex== 1 && direction == 'up')
+              ||(index == 1 && nextIndex== 5 && direction == 'down')
+              ){
+
             document.getElementById("nav-home").style["color"]="#fff";
             document.getElementById("nav-about").style["color"]="#fff";
             document.getElementById("nav-skills").style["color"]="#fff";
@@ -161,7 +111,6 @@ document.addEventListener('DOMContentLoaded', function() {
             }
           }
         }
-
     });
   });
 
